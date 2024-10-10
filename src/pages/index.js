6d60b3ba12ef1c0668e03bd7ed33d87a5e69e5bb
MateoGlzAlon <mateo.gonzalez.alonso@gmail.com>
2024-10-10 19:59:51 +0200

    removed index

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,29 +9,7 @@ import { Redirect } from 'react-router-dom'; // Import Redirect from react-route
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
   return <Redirect to="/intro" />;
 
 }
